10e5e94633d6016bdf931112f1177ed09fb86667
Ron Brogan <ron@proliensearch.com>
2016-09-11 23:10:01 -0400

    Adds missing WavefrontObj items, adds .suo to ignore.

---
+++ LetsDraw/Formats/WavefrontObj/ObjLoader.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LetsDraw.Core;
+using LetsDraw.Rendering;
+using OpenTK;
+
+namespace LetsDraw.Formats.Obj
+{
+    public class ObjLoader
+    {
+        public List<Vector3> RawVerts = new List<Vector3>();
+        public List<Vector2> TextureCoords = new List<Vector2>();
+        public List<Vector3> Normals = new List<Vector3>();
+
+        public List<ObjMesh> Meshes = new List<ObjMesh>();
+
+        public ObjLoader(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath).Select(l => l.Trim()).Where(l => !l.StartsWith("#"));
+            ObjMesh currMesh = null;
+
+            var vertexDict = new IndexedDictionary<string, VertexFormat>();
+
+            foreach(var line in lines)
+            {
+                var parts = line.Split(' ');
+
+                switch (parts[0])
+                {
+                    case "v":
+                        RawVerts.Add(new Vector3(float.Parse(parts[2]), float.Parse(parts[3]), float.Parse(parts[4])));
+                        break;
+
+                    case "vt":
+                        TextureCoords.Add(new Vector2(float.Parse(parts[1]), float.Parse(parts[2])));
+                        break;
+
+                    case "vn":
+                        Normals.Add(new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3])));
+                        break;
+
+                    case "g":
+                        if(currMesh != null)
+                        {
+                            currMesh.Verticies = vertexDict.Values;
+                            Meshes.Add(currMesh);
+                        }
+                        currMesh = new ObjMesh(parts[1]);
+                        break;
+
+                    case "f":
+                        for(int i = 1; i < 4; i++)
+                        {
+                            var indicies = parts[i].Split('/');
+
+                            var index = vertexDict.Add(parts[i], new VertexFormat(RawVerts[int.Parse(indicies[0]) - 1], TextureCoords[int.Parse(indicies[1]) - 1]));
+                            currMesh.Indicies.Add((uint)index);
+                        }
+                        currMesh.Faces++;
+                        
+
+                        break;
+
+                }
+
+            }
+            currMesh.Verticies = vertexDict.Values;
+            Meshes.Add(currMesh);
+
+        }
+
+
+    }
+}
---
+++ LetsDraw/Formats/WavefrontObj/ObjMesh.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using LetsDraw.Rendering;
+
+namespace LetsDraw.Formats.Obj
+{
+    public class ObjMesh
+    {
+        public string Name { get; set; }
+        public int Faces = 0;
+        public List<uint> Indicies = new List<uint>();
+        public List<VertexFormat> Verticies = new List<VertexFormat>();
+
+        public ObjMesh(string name)
+        {
+            Name = name;
+        }
+    }
+}
